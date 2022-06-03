a9827c173069570e57ee8cabd2fcff19890af7e2
Kevin Trejo <kevintrejo@Kevins-MacBook-Pro.local>
2022-06-03 11:57:18 -0500

    initial commit

---
+++ src/main/java/com/kevtc/hexagonalarchexample/HexagonalArchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kevtc.hexagonalarchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevtc/hexagonalarchexample/HexagonalArchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevtc.hexagonalarchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
